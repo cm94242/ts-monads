0f944925cc93b499e0bb264dfdac87f1ae497de6
chris <chris.madden@candidpartners.com>
2019-09-05 17:20:49 -0400

    tests for option

---
+++ __tests__/option.spec.ts
@@ -0,0 +1,53 @@
+import { Option, OptionMatch } from '../src/option'
+
+describe('option suite', () => {
+
+	test(`None basics`, () => {
+		const none: Option<number> = Option.from(null)
+		expect(none.isEmpty()).toBe(true)
+		expect(none.isDefined()).toBe(false)
+		expect(none.map(v => v).isDefined()).toBe(false)
+		expect(none.flatMap(v => Option.from(v)).isDefined()).toBe(false)
+		expect(none.fold('5', v => `${v}`)).toBe('5')
+		expect(none.getOrElse(5)).toBe(5)
+		expect(none.orElse(Option.from(5)).get()).toBe(5)
+		expect(none.filter(v => false).isDefined()).toBe(false)
+		expect(none.filter(v => true).isDefined()).toBe(false)
+
+		expect(none.match({
+			some: v => false,
+			none: () => true
+		})).toBe(true)
+
+		try {
+			none.get()
+			fail('Should not have been invoked')
+		} catch (err) {}
+	})
+
+	test(`Some basics`, () => {
+		const some: Option<number> = Option.from(1)
+		expect(some.isEmpty()).toBe(false)
+		expect(some.isDefined()).toBe(true)
+		expect(some.map(v => v).isDefined()).toBe(true)
+		expect(some.flatMap(v => Option.from(v)).isDefined()).toBe(true)
+		expect(some.fold('5', v => `${v}`)).toBe('1')
+		expect(some.getOrElse(5)).toBe(1)
+		expect(some.orElse(Option.from(5)).get()).toBe(1)
+		expect(some.filter(v => false).isDefined()).toBe(false)
+		expect(some.filter(v => true).isDefined()).toBe(true)
+
+		expect(some.match({
+			some: v => true,
+			none: () => false
+		})).toBe(true)
+
+		try {
+			expect(some.get()).toBe(1)
+		} catch (err) {
+			fail('Should not have been invoked')
+		}
+
+	})
+
+})
